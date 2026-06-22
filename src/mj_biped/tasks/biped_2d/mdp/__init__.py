271210560109e94ddc966bee41d76532d8d0a923
Kirill Petrekeev <65284290+t0sster@users.noreply.github.com>
2026-06-22 13:00:51 +0300

    Revert "dev: complete train"
    This reverts commit 4bde3d2f0295bf443414e693401865990cfdaf8e.

--- src/mj_biped/tasks/biped_2d/mdp/__init__.py
+++ src/mj_biped/tasks/biped_2d/mdp/__init__.py
@@ -1,5 +1,9 @@
-from .commands import *
-from .observations import *
-from .rewards import *
-from .runner import *
-from .terminations import *
+"""MDP terms for the biped_2d task."""
+
+from .rewards import alive
+from .rewards import forward_velocity
+from .rewards import joint_torque
+from .rewards import joint_velocity
+from .rewards import lateral_centering
+from .rewards import upright
+from .runner import biped_2d_ppo_runner_cfg
