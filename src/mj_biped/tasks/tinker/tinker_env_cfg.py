05a2e5c0f08bd542823a095853586ecce3641ad1
etrnlcoma <ilya200423il@mail.ru>
2026-08-10 16:09:09 +0300

    dev: change std  vel x reward

--- src/mj_biped/tasks/tinker/tinker_env_cfg.py
+++ src/mj_biped/tasks/tinker/tinker_env_cfg.py
@@ -269,7 +269,7 @@ def _make_env_cfg(num_envs: int) -> ManagerBasedRlEnvCfg:
       params={
         "asset_cfg": _ROBOT_CFG,
         "command_name": "velocity",
-        "std": math.sqrt(0.02),
+        "std": math.sqrt(0.05),
       },
     ),
     "track_angular_velocity": RewardTermCfg(
