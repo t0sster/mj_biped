4a9032887d08faa4eaba7acb9756692d55e9c54e
etrnlcoma <ilya200423il@mail.ru>
2026-08-18 18:03:29 +0300

    dev: change rewards for leg period

--- src/mj_biped/tasks/tinker/tinker_env_cfg.py
+++ src/mj_biped/tasks/tinker/tinker_env_cfg.py
@@ -353,7 +353,7 @@ def _make_env_cfg(num_envs: int) -> ManagerBasedRlEnvCfg:
     ),
     "swing_foot_force": RewardTermCfg(
       func=mdp.swing_foot_force_l2,
-      weight=-0.75,
+      weight=-1.5,
       params={
         "command_name": "gait",
         "motion_command_name": "velocity",
@@ -364,7 +364,7 @@ def _make_env_cfg(num_envs: int) -> ManagerBasedRlEnvCfg:
     ),
     "stance_foot_velocity": RewardTermCfg(
       func=mdp.stance_foot_velocity_l2,
-      weight=-0.5,
+      weight=-1.0,
       params={
         "asset_cfg": _FOOT_SITE_CFG,
         "command_name": "gait",
