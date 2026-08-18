81c20c4edfbc06bfb548a8fb953cdf11ad64ddeb
etrnlcoma <ilya200423il@mail.ru>
2026-08-18 18:20:21 +0300

    dev: change target height

--- src/mj_biped/tasks/tinker/tinker_env_cfg.py
+++ src/mj_biped/tasks/tinker/tinker_env_cfg.py
@@ -292,10 +292,10 @@ def _make_env_cfg(num_envs: int) -> ManagerBasedRlEnvCfg:
   rewards = {
     "base_height_track": RewardTermCfg(
       func=mdp.base_height,
-      weight=-2.0,
+      weight=-1.5,
       params={
         "asset_cfg": _ROBOT_CFG,
-        "target_height": 0.28,
+        "target_height": 0.25,
       }
     ),
 
