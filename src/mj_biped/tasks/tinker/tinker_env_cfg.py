c0476be79cde8577aa61189e11e67aa7aa56e7fe
Ilya <ilya200423il@mail.ru>
2026-08-20 17:10:25 +0300

    dev: increase more vel_x

--- src/mj_biped/tasks/tinker/tinker_env_cfg.py
+++ src/mj_biped/tasks/tinker/tinker_env_cfg.py
@@ -301,7 +301,7 @@ def _make_env_cfg(num_envs: int) -> ManagerBasedRlEnvCfg:
 
     "track_linear_velocity": RewardTermCfg(
       func=velocity_mdp.track_linear_velocity,
-      weight=1.75,
+      weight=2.5,
       params={
         "asset_cfg": _ROBOT_CFG,
         "command_name": "velocity",
