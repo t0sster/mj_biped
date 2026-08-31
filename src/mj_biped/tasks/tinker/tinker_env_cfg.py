c8cfcee5ef3f4afc000f7286ef186fb856019857
Ilya <ilya200423il@mail.ru>
2026-08-31 13:18:36 +0300

    dev: changed some rew

--- src/mj_biped/tasks/tinker/tinker_env_cfg.py
+++ src/mj_biped/tasks/tinker/tinker_env_cfg.py
@@ -273,12 +273,12 @@ def _make_env_cfg(num_envs: int) -> ManagerBasedRlEnvCfg:
       resampling_time_range=(4.0, 8.0),
       rel_standing_envs=0.15,
       rel_forward_envs=0.3,
-      heading_command=True,
+      heading_command=False,
       ranges=velocity_mdp.UniformVelocityCommandCfg.Ranges(
         lin_vel_x=(-0.15, 0.15),
         lin_vel_y=(-0.1, 0.1),
         ang_vel_z=(-0.15, 0.15),
-        heading=(-math.pi, math.pi),
+        # heading=(-math.pi, math.pi),
       ),
     ),
     "gait": mdp.UniformGaitCommandCfg(
@@ -312,7 +312,7 @@ def _make_env_cfg(num_envs: int) -> ManagerBasedRlEnvCfg:
 
     "track_linear_velocity": RewardTermCfg(
       func=velocity_mdp.track_linear_velocity,
-      weight=2.25,
+      weight=2.1,
       params={
         "asset_cfg": _ROBOT_CFG,
         "command_name": "velocity",
@@ -364,12 +364,12 @@ def _make_env_cfg(num_envs: int) -> ManagerBasedRlEnvCfg:
     ),
     "gait_contact_match": RewardTermCfg(
       func=mdp.gait_contact_match,
-      weight=0.75,
+      weight=1.0,
       params={
         "command_name": "gait",
         "motion_command_name": "velocity",
         "sensor_name": _FEET_CONTACT_SENSOR,
-        "command_threshold": 0.05,
+        "command_threshold": 0.025,
       },
     ),
     "swing_foot_force": RewardTermCfg(
